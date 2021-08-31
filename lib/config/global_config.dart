5283f956c22ae3afb807cf48712af05ee92356b9
shaolonger <shaolong.chuang@qq.com>
2021-08-31 12:05:11 +0800

    feat: 修改全局配置信息

--- lib/config/global_config.dart
+++ lib/config/global_config.dart
@@ -1,25 +1,25 @@
 /// 全局配置类，用于存放全局配置相关的常量，便于个性化定义
 class GlobalConfig {
   /// 启动页的标题（应用名称）
-  static const APP_NAME_FULL = "Web Monitor App";
+  static const APP_NAME_FULL = "Bit Monitor App";
 
   /// 应用名称（短）
-  static const APP_NAME_SHORT = "Web Monitor";
+  static const APP_NAME_SHORT = "Bit Monitor";
 
   /// 应用代号
-  static const APP_NAME_CODE = "WM";
+  static const APP_NAME_CODE = "BM";
 
   /// 应用简介（短）
-  static const APP_INTRODUCTION = "A bit_monitor_app platform for both mobile and "
+  static const APP_INTRODUCTION = "A monitor platform for both mobile and "
       "desktop web applications";
 
   /// 应用简介（全）
-  static const APP_INTRODUCTION_FULL = "A bit_monitor_app platform for both mobile and "
-      "desktop web applications using Java/Angular/React/vue and other languages.";
+  static const APP_INTRODUCTION_FULL = "A monitor platform for both mobile and "
+      "desktop web applications using Java/Angular/React/Vue and other languages.";
 
   /// 开源地址
   static const APP_GITHUB_ADDRESS =
-      "项目开源地址：https://github.com/bit-bit_monitor_app/bit-bit_monitor_app-app-flutter";
+      "项目开源地址：https://github.com/bit-monitor/bit-monitor-app-flutter";
 
   /// 如果觉得好用
   static const APP_GITHUB_REMARK =
